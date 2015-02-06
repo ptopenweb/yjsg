5ba85f5b5b29ddf1d76a2343df65aef358afbc04
OpenWeb <webmail@openweb.pt>
2015-02-06 09:57:52 +0000

    Some problems with shortcode framework.php
    Read the code comments and changes

--- includes/yjsgshortcodes/templates/framework.php
+++ includes/yjsgshortcodes/templates/framework.php
@@ -20,7 +20,15 @@ jimport('joomla.filesystem.folder');
 jimport('joomla.plugin.helper');
 jimport('joomla.session.session');
 
-$mainframe 				= JFactory::getApplication('site');
+// Attention: is reading site language. backend language could be other
+// since shortcodes are available in frontend (edit) and backend
+// it really should detect which site type is the user using
+// if you change the sitetype var from 'site' to 'administrator'
+// it will read the admin site language.
+// but how to detect if user is using backend or frontend?
+// Also and if the user have 2 or more languages on frontend, what language will it choose?
+$sitetype='site';
+$mainframe 				= JFactory::getApplication($sitetype);
 $mainframe->initialise();
 if (intval(JVERSION) >= 3) {
 	JPluginHelper::importPlugin('system','yjsg');
@@ -29,4 +37,12 @@ $session 				= JFactory::getSession();
 $user 					= JFactory::getUser();
 $language				= JFactory::getLanguage();
 $base_link				= preg_replace('/(\btemplates\b|\bmodules\b|\bcomponents\b|\bplugins\b)(.*)/','',JURI::root());
-$language->setLanguage(JComponentHelper::getParams('com_languages')->get('site'));
+
+print_r($mainframe);
+
+$language->setLanguage(JComponentHelper::getParams('com_languages')->get($sitetype));
+// Sometimes it needed to be forced after changing the language or will appear in english
+$language->load('joomla', JPATH_ADMINISTRATOR);
+$language->load('plg_system_yjsg', JPATH_ADMINISTRATOR);
+// Should also read the template language files
+$language->load('tpl_'.Yjsg::getDefaultTemplate(), JPATH_SITE);
